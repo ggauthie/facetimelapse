65a23c5bf731bfa7960fc8c734f6e374e6ddb2de
Gwenael Jestin <gwenael@nc-ass-vip.sdv.fr>
2020-03-01 18:47:55 +0100

    Initial commit

---
+++ stuctures.h
@@ -0,0 +1,35 @@
+//
+//  stuctures.h
+//  
+//
+//  Created by Gwenael Jestin on 29/02/2020.
+//
+
+#ifndef stuctures_h
+#define stuctures_h
+
+typedef struct {
+    unsigned char blue;
+    unsigned char green;
+    unsigned char red;
+} PixelRGB;             /* Pixel in Red Green Blue */
+                        /* Could be modified to another type of color space */
+
+typedef struct {
+    unsigned char value;
+} PixelBW;              /* Pixel in Black & White */
+
+typedef struct {
+    int width;          /* Nb of columns */
+    int height;         /* Nb of rows */
+    PixelRGB * ptrPixel;               /* Pixels of the image */
+} ImageRGB;
+
+typedef struct {
+    int width;          /* Nb of columns */
+    int height;         /* Nb of rows */
+    PixelBW* ptrPixel;               /* Pixels of the image */
+} ImageBW;
+
+
+#endif /* stuctures_h */
