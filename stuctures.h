b4767d7b4cab3f1f362dfb066cc254caa9a5603b
Jestin Gwenael <gwenael.jestin@insa-rennes.fr>
2020-03-09 19:31:35 +0100

    Removed structures PixelBW and ImageBW for more global types Pixel and Image

--- stuctures.h
+++ stuctures.h
@@ -12,18 +12,15 @@ typedef struct {
     unsigned char blue;
     unsigned char green;
     unsigned char red;
-} PixelRGB;             /* Pixel in Red Green Blue */
+} Pixel;             /* Pixel in Red Green Blue */
                         /* Could be modified to another type of color space */
 
-typedef struct {
-    unsigned char value;
-} PixelBW;              /* Pixel in Black & White */
 
 typedef struct {
     int width;          /* Nb of columns */
     int height;         /* Nb of rows */
-    PixelRGB * ptrPixel;               /* Pixels of the image */
-} ImageRGB;
+    Pixel * ptrPixel;               /* Pixels of the image */
+} Image;
 
 
 
