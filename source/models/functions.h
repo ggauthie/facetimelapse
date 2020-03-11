5a7330b05c00ca0a6cdc8aee01f6a8b74adf7c34
Gauthier Guillaume <guillaume.gauthier@insa-rennes.fr>
2020-03-11 16:19:14 +0100

    Update functions.h

--- source/models/functions.h
+++ source/models/functions.h
@@ -8,7 +8,7 @@ ImageRGB * createImage(int width, int height); /*création d'image noir et blanc
 void freeImage(ImageRGB * image); /*libération mémoire image noir et blanc*/
 ImageRGB * color_to_WB(ImageRGB * image);/*conversion image couleur vers noir et blanc*/
 PixelRGB getPixel(ImageRGB * image, int width, int height);/*recupération d'un pixel noir et blanc avec ses coordonnées*/
-void modifyPixeltoWB(ImageRGB * image, int witdh, int height, PixelRGB pixel); /*modification d'un pixel noir et blanc / si image couleur PixelRGB*/
+void modifyPixelToWB(ImageRGB * image, int witdh, int height, PixelRGB pixel); /*modification d'un pixel noir et blanc / si image couleur PixelRGB*/
 Image[] imagesInter(ImageBW * image1, ImageRGB * image2, int nombre_image); /*Renvoi un tableau d'image intermédiaire à partir de deux images et du nombre voulu*/
 int calculateWeight (int nbImage, int numeroImage); /*calcul du poids de l'image*/
 
