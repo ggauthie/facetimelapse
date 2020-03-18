fd4b0078584615ce8863d45b79bc5c3e2c4fd07e
Gauthier Guillaume <guillaume.gauthier@insa-rennes.fr>
2020-03-18 22:22:29 +0100

    Update functions.h

--- source/models/functions.h
+++ source/models/functions.h
@@ -4,12 +4,12 @@
 #include "stuctures.h"
 
 
-ImageRGB * createImage(int width, int height); /*création d'image noir et blanc/RGB*/
-void freeImage(ImageRGB * image); /*libération mémoire image noir et blanc*/
-ImageRGB * color_to_WB(ImageRGB * image);/*conversion image couleur vers noir et blanc*/
-PixelRGB getPixel(ImageRGB * image, int width, int height);/*recupération d'un pixel noir et blanc avec ses coordonnées*/
-void modifyPixelToWB(ImageRGB * image, int witdh, int height, PixelRGB pixel); /*modification d'un pixel noir et blanc / si image couleur PixelRGB*/
-Image[] imagesInter(ImageBW * image1, ImageRGB * image2, int nombre_image); /*Renvoi un tableau d'image intermédiaire à partir de deux images et du nombre voulu*/
+Image * createImage(int width, int height); /*création d'image noir et blanc/RGB*/
+void freeImage(Image * image); /*libération mémoire image noir et blanc*/
+Image * color_to_WB(Image * image);/*conversion image couleur vers noir et blanc*/
+PixelRGB getPixel(Image * image, int width, int height);/*recupération d'un pixel noir et blanc avec ses coordonnées*/
+void modifyPixelToWB(Image * image, int witdh, int height, PixelRGB pixel); /*modification d'un pixel noir et blanc / si image couleur PixelRGB*/
+Image[] imagesInter(Image * image1, Image * image2, int nombre_image); /*Renvoi un tableau d'image intermédiaire à partir de deux images et du nombre voulu*/
 int calculateWeight (int nbImage, int numeroImage); /*calcul du poids de l'image*/
 
 //Fonction conversion
