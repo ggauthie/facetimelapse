c67e161ca96be1aeb73295ed05b00aec7fc4d932
Ramel Esteban <esteban.ramel@insa-rennes.fr>
2020-03-19 16:01:09 +0100

    Update functions.c (imagesInter pour qu'elle renvoie pointeur)

--- source/models/functions.c
+++ source/models/functions.c
@@ -46,7 +46,7 @@ void  freeImage(Image * image)
     free(image);
 }
 
-Image[] imagesInter(Image * image1, Image * image2, int nombre_image){
+Image* imagesInter(Image * image1, Image * image2, int nombre_image){
     int i=0;
     Image* tab_images = (Image*)malloc(nombre_image*sizeof(Image));
     for(i;i<nombre_image;i++){
