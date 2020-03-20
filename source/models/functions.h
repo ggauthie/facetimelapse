4a23be132b4eae58af9f3ff8b8d05bc71ef2e159
Hamon William <william.hamon@insa-rennes.fr>
2020-03-20 16:02:36 +0100

    Update functions.h

--- source/models/functions.h
+++ source/models/functions.h
@@ -5,14 +5,21 @@
 
 
 Image * createImage(int width, int height); /*création d'image noir et blanc/RGB*/
+
 void freeImage(Image * image); /*libération mémoire image noir et blanc*/
+
 Image * color_to_WB(Image * image);/*conversion image couleur vers noir et blanc*/
-PixelRGB getPixel(Image * image, int width, int height);/*recupération d'un pixel noir et blanc avec ses coordonnées*/
-void modifyPixelToWB(Image * image, int witdh, int height, PixelRGB pixel); /*modification d'un pixel noir et blanc / si image couleur PixelRGB*/
-Image[] imagesInter(Image * image1, Image * image2, int nombre_image); /*Renvoi un tableau d'image intermédiaire à partir de deux images et du nombre voulu*/
-int calculateWeight (int nbImage, int numeroImage); /*calcul du poids de l'image*/
+
+Pixel getPixel(Image * image, int width, int height);/*recupération d'un pixel noir et blanc avec ses coordonnées*/
+
+Pixel modifyPixel_to_WB(Pixel pixel); /*modification d'un pixel noir et blanc*/
+
+Image* imagesInter(Image * image1, Image * image2, int nombre_image); /*Renvoi un tableau d'image intermédiaire à partir de deux images et du nombre voulu*/
+
+float calculateWeight (int nbImage, int numeroImage); /*calcul du poids de l'image*/
 
 //Fonction conversion
+
 Image* readBMPFile(char* filename, int verbose);
 void writeBMPFile(char* filename, Image* im, int verbose);
 
