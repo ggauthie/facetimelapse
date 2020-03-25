71c35589d287c02b83b94d2d67372e0617f0726c
Gauthier Guillaume <guillaume.gauthier@insa-rennes.fr>
2020-03-25 19:15:31 +0100

    Update test.c

--- source/models/test.c
+++ source/models/test.c
@@ -7,9 +7,25 @@
 
 #define MY_FILENAME_MAX 16
 
-int main()
-{
-    
+int test_create_image(Image* image){
+    if(image == NULL){
+        return -1;
+    }else{
+        return 0;
+    }
+}
 
-    return 0;
+
+void print_values_RGB(Pixel* pixel){
+    printf( "VALEURS R: %u /G: %u /B %u \n", pixel->red, pixel->green, pixel->blue);
+} 
+
+void print_values_Image(Image * image){
+    int i=0;
+    int nb_total_pixel = (image->width)*(image->height);
+    if(image == NULL){
+    for(i; i<nb_total_pixel; i++){
+        print_values_RGB(image->ptrPixel[i]);
+    }
+    }
 }
