214e3b1b4e3ff457c3c785eb8722ec65419d358d
Gauthier Guillaume <guillaume.gauthier@insa-rennes.fr>
2020-03-25 19:19:48 +0100

    Update test.h

--- source/models/test.h
+++ source/models/test.h
@@ -4,7 +4,7 @@
 #include "functions.h"
 #include "structures.h"
 
-
+int test_same_image(Image* image1, Image* image2);
 int test_create_image(Image* image); //La fonction retourne 0 si l'image est créée sinon -1
 void print_values_RGB(Pixel* pixel); //La fonction affiche les valeurs RGB du pixel pixel
 void print_values_Image(Image * image); //La fonction affiche les valeurs RGB des pixels de l'image
