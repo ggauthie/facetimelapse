12955624ad3ecf57c95b1ef74c5504792d2a6cca
Ramel Esteban <esteban.ramel@insa-rennes.fr>
2020-03-25 19:17:25 +0100

    Update test.h

--- source/models/test.h
+++ source/models/test.h
@@ -4,7 +4,7 @@
 #include "functions.h"
 #include "structures.h"
 
-void test_compare_pixel(Pixel a, Pixel b);
+
 int test_create_image(Image* image); //La fonction retourne 0 si l'image est créée sinon -1
 void print_values_RGB(Pixel* pixel); //La fonction affiche les valeurs RGB du pixel pixel
 void print_values_Image(Image * image); //La fonction affiche les valeurs RGB des pixels de l'image
