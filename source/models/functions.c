ee0d6b21d5c450b03fc71de786ccfc3060a0b6c8
Gauthier Guillaume <guillaume.gauthier@insa-rennes.fr>
2020-03-26 14:42:42 +0100

    Update functions.c

--- source/models/functions.c
+++ source/models/functions.c
@@ -1,28 +1,40 @@
-#include "stuctures.h"
+#include "structures.h"
 #include "functions.h"
+#include <math.h>
 
 
 Image * createImage(int width, int height){
     Image *new_image = (Image*) malloc(sizeof(Image));
-    new_image->width = h;
-    new_image->height = l;
-    new_image->ptrPixel = (Pixel*)malloc(l*h*sizeof(Pixel));
+    new_image->width = width;
+    new_image->height = height;
+    new_image->ptrPixel = (Pixel*)malloc(width*height*sizeof(Pixel));
     return new_image;
 }
+/*GENERER UNE IMAGE ALEATOIRE
 
-Pixel * modifyPixel_to_WB(Pixel* pixel){
+Image * generate_random_image(int width, int height){
+    Image *new_image = (Image*) malloc(sizeof(Image));
+    new_image=createImage(int width, int height);
+    int taille_image = width*height;
+    for(int i=0;i<taille_image;i++){
+        new_image->ptrPixel->blue=rand()
+    }
+*/
+}
+Pixel* modifyPixel_to_WB(Pixel* pixel){
 	unsigned char GRIS;
-	GRIS = 0.2125*(pixel->red)+0.7154*(pixel->green)+0.0721*(pixel->blue);
+	GRIS = floor(0.2125*(pixel->red)+0.7154*(pixel->green)+0.0721*(pixel->blue));
 	pixel->blue = GRIS;
 	pixel->green= GRIS;
- 	pixel->red  = GRIS; 
+ 	pixel->red  = GRIS;
  	return pixel;
 }
 
-	
 void setPixel(Image* image, int x, int y, Pixel* pixel){
-	image->ptrPixel[x*image->width +y] = pixel;
-    
+	image->ptrPixel[x*image->width +y].blue=pixel->blue;
+	image->ptrPixel[x*image->width +y].red=pixel->red;
+	image->ptrPixel[x*image->width +y].green=pixel->green;
+
 }
 
 Image * color_to_WB(Image * image){
@@ -32,7 +44,7 @@ Image * color_to_WB(Image * image){
 	Pixel* pixel =(Pixel*)malloc(sizeof(Pixel));
     for(i; i<= image->width; i++){
 	for(j; j<= image->height; j++){
-	 pixel = modifypixel_to_WB(image->ptrPixel[i*(image->width)+j]);   
+	 pixel = modifypixel_to_WB(image->ptrPixel[i*(image->width)+j]);
 	 setPixel(image,i,j,pixel);
 	 }
 	}
@@ -51,30 +63,31 @@ Image* imagesInter(Image * image1, Image * image2, int nombre_image){
     int j=0;
     int k=0;
 	float a,b,c,d,e,f;
-	
+
     Image* tab_images = (Image*)malloc(nombre_image*sizeof(Image));
-    for(i;i<=nombre_image;i++){ 
+    for(i;i<=nombre_image;i++){
         for(j; j<image1->width; j++){
         for(k; k<image1->height;k++){
-            
+
             Pixel *pixel =(Pixel*)malloc(sizeof(Pixel));
-            
-            a=calculateWeight(nombre_image,i)*image1->ptrPixel[j*(image1->width)+k]->blue;
-		    b=calculateWeight(nombre_image,i)*image1->ptrPixel[j*(image1->width)+k]->green;
-		    c=calculateWeight(nombre_image,i)*image1->ptrPixel[j*(image1->width)+k]->red;
-		    d=(1-calculateWeight(nombre_image,i))*image2->ptrPixel[j*(image1->width)+k]->blue;
-		    e=(1-calculateWeight(nombre_image,i))*image2->ptrPixel[j*(image1->width)+k]->green;
-		    f=(1-calculateWeight(nombre_image,i))*image2->ptrPixel[j*(image1->width)+k]->red;
+
+            a=calculateWeight(nombre_image,i)*image1->ptrPixel[j*(image1->width)+k].blue;
+		    b=calculateWeight(nombre_image,i)*image1->ptrPixel[j*(image1->width)+k].green;
+		    c=calculateWeight(nombre_image,i)*image1->ptrPixel[j*(image1->width)+k].red;
+		    d=(1-calculateWeight(nombre_image,i))*image2->ptrPixel[j*(image1->width)+k].blue;
+		    e=(1-calculateWeight(nombre_image,i))*image2->ptrPixel[j*(image1->width)+k].green;
+		    f=(1-calculateWeight(nombre_image,i))*image2->ptrPixel[j*(image1->width)+k].red;
 		    pixel->blue=(a+d)/2;
 		    pixel->green=(b+e)/2;
 		    pixel->red=(c+f)/2;
-		    setPixel(tab_images[i], j, k, pixel)
+		    setPixel(tab_images, j, k, pixel);
+		    tab_images++;
     }
 }
 }
 	return tab_images;
 }
-    
+
 /*Renvoi un tableau d'image intermédiaire à partir de deux images et du nombre voulu*/
 float calculateWeight (int nbImage, int numeroImage){
     return (numeroImage/nbImage+1); //nbImage commence à 1
