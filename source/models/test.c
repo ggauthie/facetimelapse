14d1b55241112a2e7f3034d104c3f810e7f1a272
Gauthier Guillaume <guillaume.gauthier@insa-rennes.fr>
2020-04-02 13:04:30 +0200

    Update test.c

--- source/models/test.c
+++ source/models/test.c
@@ -1,38 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cmocka.h>
 
 #include "structures.h"
 #include "functions.h"
 
-int test_create_image(Image* image){
-    if(image == NULL){
-        return -1;
-    }else{
-        return 0;
-    }
+static void test_createImage(void **state){
+	Image *image = (Image*) malloc(sizeof(Image));
+	image=createImage(10,20);
+   	assert_int_equal (image->width,10);
+	assert_int_equal (image->height,20);
+    
 }
 
-int test_same_image(Image* image1, Image* image2){
-    if(image1!=NULL && image2!=NULL){
-        if((image1->width==image2->width) && (image1->height==image2->height)){
-            return 0;
-        }else{
-            return -1;
-        }
-    }
+static void test_create_pixel(void **state){
+	Pixel* pixel =(Pixel*)malloc(sizeof(Pixel));
+	pixel = create_pixel(128,255,75);
+   	assert_int_equal (pixel->red,128);
+	assert_int_equal (pixel->green,256);
+	assert_int_equal (pixel->blue,75);
+    
+}
+
+*/test imageInter*/
+*/test colortoWB */
+
+static void test_modifyPixel_to_WB(void **state){
+	Pixel* pixel =(Pixel*)malloc(sizeof(Pixel));
+	pixel = modifyPixel(create_pixel(128,256,75));
+	
+	assert_int_equal (pixel->blue,215);
+	assert_int_equal (pixel->green,215);
+	assert_int_equal (pixel->red,215);
+	
+}
+
+static void test_setPixel(void **state){
+
+	Pixel* pixel =(Pixel*)malloc(sizeof(Pixel));
+	image* image =(Image*)malloc(sizeof(Pixel));
+
+	pixel=create_pixel(128,255,75);
+	image=create_image(10,20);
+
+	setPixel(image,5, 10, pixel);
+	
+	assert_int_equal (image->ptrPixel[5*10+10]->blue,pixel->blue);
+	assert_int_equal (image->ptrPixel[5*10+10]->green,pixel->green);
+	assert_int_equal (image->ptrPixel[5*10+10]-red,pixel->red);
+	
+	assert_int_equal (setPixel(image,50, 12, pixel),-1);
+	
+	
+}
+
+static void test_freeImage(void **state){
+	Image *image = (Image*) malloc(sizeof(Image));
+	image=createImage(10,20);
+	
+	freeImage(image);
+	assert_null (image); //vérifie si le pointeur image est NULL
 }
 
 
-void print_values_RGB(Pixel pixel){
-    printf( "VALEURS R: %u /G: %u /B %u \n", pixel.red, pixel.green, pixel.blue);
+static void test_calculateWeight(void **state){
+	void assert_float_equal	(calculateWeight (10, 1),0.09, 0.001);
+	
+	
 }
 
-void print_values_Image(Image * image){
-    int i=0;
-    int nb_total_pixel = (image->width)*(image->height);
-    if(image != NULL){
-    for(i; i<nb_total_pixel; i++){
-        print_values_RGB(image->ptrPixel[i]);
-    }
-    }
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_createImage),
+    	cmocka_unit_test(test_create_pixel),
+    	cmocka_unit_test(test_modifyPixel_to_WB),
+	    cmocka_unit_test(test_setPixel),
+	    cmocka_unit_test(test_freeImage)
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
 }
