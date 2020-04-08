e25f07b5a49faa5db18dea2d307d96f93acfc2e4
Hamon William <william.hamon@insa-rennes.fr>
2020-04-08 11:53:42 +0200

    fichier.c pour ouvrir un fichier

---
+++ source/program/fichier.c
@@ -0,0 +1,24 @@
+#include "fichier.h"
+
+
+FILE* ouvrirFichier(char* nom, char* mode, TypeRetour t)
+{
+    FILE* fic = fopen(nom, mode);
+
+    if(fic == NULL)
+    {
+        if(t == ARRET)
+            exit(1);
+        return NULL;
+    }
+
+    return fic;
+}
+
+int fermerFichier(FILE* f)
+{
+    if(f == NULL)
+        return 1;
+
+    return fclose(f);
+}
