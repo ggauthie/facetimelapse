a70e3255ceb3072196c5805536f76165985b06d4
Gauthier Guillaume <guillaume.gauthier@insa-rennes.fr>
2020-04-16 18:32:11 +0200

    Grosse MAJ des fonctions tests, il ne reste que la fonction test d'ImageInter

--- source/models/test.c
+++ source/models/test.c
@@ -14,33 +14,48 @@
 
 
 static int setup(void **state){
-
+    
     Image *image_1 = createImage(3,4);
-    Image *image_2 = createImage(3,4);
-    if(image_1 ==NULL || image_2==NULL || image_1->ptrPixel ==NULL || image_2->ptrPixel==NULL){
-        return -1;
-    }
-    for(int i=0;i<(image_1->width)*(image_1->height);i++){
-        image_1->ptrPixel[i];// a aucun effet sur l'image
-    }
-
-    for(int i=0;i<(image_2->width)*(image_2->height);i++){
-        image_2->ptrPixel[i];// a aucun effet sur l'image
-    }
+   
+    *state = image_1;
     return 0;
 }
+/*
+static int setup_1(void **state){
+    
+    Image *image_2 = createImage(3,3);
+    Image *image_3 = createImage(3,3);
+
+    Pixel pixel_1 ={0,0,0};
+    Pixel pixel_2={255,255,255};
+    
+    int i,j;
+
+    for(i=0; i< 3; i++){
+	     for(j=0; j< 3; j++){
+		setPixel(image_1,i,j,pixel_1);
+		setPixel(image_2,i,j,pixel_2);
+	      }
+}
+Image *tab[]={image_2,image_3};
+*state =tab;
+return 0;
+}*/
 
 static int teardown(void **state){
-    Image *image_1 = createImage(3,4); //modif
-    Image *image_2 = createImage(3,4); //modif
-    freeImage(image_1);
-    freeImage(image_2);
+    free(((Image*)*state)->ptrPixel);
+    free(*state);
     return 0;
 }
 
+/*static int teardown_1(void **state){
+    free(((Image*)*state)->ptrPixel);
+    free(*state);
+    return 0;
+}*/
+
 static void test_createImage(void **state){
-  Image *image_1 = createImage(3,4); //modif
-  //Image *image_2 = createImage(3,4); //modif
+	Image *image_1 = createImage(3,4);
 	assert_int_equal (image_1->width,3);
 	assert_int_equal (image_1->height,4);
 
@@ -48,14 +63,17 @@ static void test_createImage(void **state){
 
 
 /*test imageInter*/
-
+/*
 static void test_imageInter(void **state){
-    int i=0,j=0;
-    Image *image_1 = createImage(3,4); //modif
-    Image *image_2 = createImage(3,4); //modif
+
+    Image** tab_images =(Image**)(*state);
+    
+    int i,j;
+
+    
     Image* tab_images = imagesInter(image_1, image_2, 3);
 
-         for(i; i<(image_1->width)*(image_1->height);i++){
+         for(i=0; i<(image_1->width)*(image_1->height);i++){
 
              assert_memory_equal((tab_images[0]).ptrPixel[i].blue,63,1);
              assert_memory_equal((tab_images[0]).ptrPixel[i].red,63,1);
@@ -71,87 +89,63 @@ static void test_imageInter(void **state){
 
         }
 
-}
+}*/
 
 /*test colortoWB */
 
 static void test_colortoWB(void **state){
-    //si test set pixel correct
-    Image *image_1 = createImage(3,4); //modif
-    Image *image_2 = createImage(3,4); //modif
-	Pixel pixel_1 ={255,0,0}; //modif
+	Image* image_1 =(Image*)(*state);	
+	Pixel pixel_1 ={255,120,120}; 
 	int i,k;
+
     for(i=0; i<(image_1->width);i++){
 		for(k=0; k<(image_1->height);k++){
 			setPixel(image_1,i, k, pixel_1);}}
-
-	assert_ptr_equal(color_to_WB(image_2),image_1); //mùodif
-}
-
-static void test_modifyPixel_to_WB(void **state){
-/*
-  Image *image_1 = createImage(3,4);
-  image_1->ptrPixel->blue = 128;
-  image_1->ptrPixel->green = 255;
-  image_1->ptrPixel->red = 75;*/
-
-  Pixel pixel_1 = {128,255,75};
-	Pixel * pixel_2 = modifyPixel_to_WB(pixel_1);
-
-	assert_int_equal(pixel_2->blue,215);
-	assert_int_equal(pixel_2->green,215);
-  assert_int_equal(pixel_2->red,215);
+    color_to_WB(image_1);
+	for(i=0; i<(image_1->width);i++){
+		for(k=0; k<(image_1->height);k++){
+			assert_int_equal(image_1->ptrPixel[k*(image_1->width)+i].red,148);
+			assert_int_equal(image_1->ptrPixel[k*(image_1->width)+i].green,148);
+			assert_int_equal(image_1->ptrPixel[k*(image_1->width)+i].blue,148);
 
 }
+}
+}
+
+
 
 static void test_setPixel(void **state){
-  Image *image_1 = createImage(3,4); //modif
-  //Image *image_2 = createImage(3,4); //modif
-    Pixel pixel_1 ={128,255,75};// c'était écrit 256 au lieu de 255
+	Image* image_1 =(Image*)(*state);
+	Pixel pixel_1 ={100,255,100};
 
 	setPixel(image_1,2, 2, pixel_1);
 
-	assert_int_equal (image_1->ptrPixel[2*image_1->width+2].blue,pixel_1.blue);
-	assert_int_equal (image_1->ptrPixel[2*image_1->width+2].green,pixel_1.green);
-	assert_int_equal (image_1->ptrPixel[2*image_1->width+2].red,pixel_1.red);
-// pas compris la ligne en dessous
-	//assert_int_equal (setPixel(image_1, 2, 2, pixel_1),-1);// image_1 ou image_2 // pixel ?? pixel_1 ??
+	assert_int_equal(image_1->ptrPixel[2*image_1->width+2].blue,pixel_1.blue);
+	assert_int_equal(image_1->ptrPixel[2*image_1->width+2].green,pixel_1.green);
+	assert_int_equal(image_1->ptrPixel[2*image_1->width+2].red,pixel_1.red);
 
 
 }
 
 static void test_getPixel(void **state){
-  Image *image_1 = createImage(3,4); //modif
-  //Image *image_2 = createImage(3,4); //modif
+    Image* image_1 =(Image*)(*state);
     Pixel *pixel = getPixel(image_1, 2, 1);
-    assert_ptr_equal(pixel, &image_1->ptrPixel[2*image_1->width + 1]);//modif
+    assert_ptr_equal(pixel, &image_1->ptrPixel[2*image_1->width + 1]);
 }
 
 
-static void test_freeImage(void **state){
-  Image *image_1 = createImage(3,4); //modif
-//  Image *image_2 = createImage(3,4); //modif
-	freeImage(image_1);
-
-	assert_null(image_1); //vérifie si le pointeur image est NULL
-}
-
- /*j'arrive pas à récupérer assert_float_equal
 static void test_calculateWeight(void **state){
-	assert_float_equal(calculateWeight (10, 1),0.09, 0.001);
+	assert_int_equal(calculateWeight(9, 2)*10,0.2*10);
 
-}*/
+}
 
 int main(void) {
     const struct CMUnitTest tests[] = {
-      cmocka_unit_test_setup_teardown(test_createImage, setup, teardown),
-    	cmocka_unit_test_setup_teardown(test_modifyPixel_to_WB, setup, teardown),
-	    cmocka_unit_test_setup_teardown(test_setPixel,setup, teardown),
+      cmocka_unit_test(test_createImage),
+	 cmocka_unit_test_setup_teardown(test_setPixel,setup, teardown),
 	    cmocka_unit_test_setup_teardown(test_getPixel,setup, teardown),
-	    cmocka_unit_test_setup_teardown(test_freeImage,setup, teardown),
 	    cmocka_unit_test_setup_teardown(test_colortoWB,setup, teardown),
-	    cmocka_unit_test_setup_teardown(test_imageInter,setup, teardown),
-	   // cmocka_unit_test_setup_teardown(test_calculateWeignt,setup, teardown)
+	    cmocka_unit_test_setup_teardown(test_calculateWeight,setup, teardown)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
